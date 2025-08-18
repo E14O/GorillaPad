3e3607a1ce060ceee6428d9c2f5eefd72b85f136
Spankypluh <liamkasia2010@gmail.com>
2025-08-18 15:11:10 -0400

    very small

---
+++ GorillaPad/GorillaPad/Functions/Apps/ScorteboardApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GorillaPad.Interfaces;
+
+namespace GorillaPad.Functions.Apps
+{
+    internal class ScorteboardApp : AppModule
+    {
+        // App That Cannot Be Removed PLZZZZ CAN WE MAKE THIS A DEFAULT APP GUYSSSS
+        public override string AppName => "Scoreboard";
+        public override string AppVersion => "0.0.1";
+    }
+}
