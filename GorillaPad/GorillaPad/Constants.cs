2c2cfb4b908e064480fee18e75c4278e59be8354
E14O <Stan19marvel@icloud.com>
2025-08-24 09:37:22 +0100

    better left hand position

--- GorillaPad/GorillaPad/Constants.cs
+++ GorillaPad/GorillaPad/Constants.cs
@@ -19,7 +19,7 @@ namespace GorillaPad
         public static readonly TransformState Chest = new(new Vector3(0.0121f, 0.1969f, 0.1303f), Quaternion.Euler(0f, 0f, 270f), new Vector3(.09f, .09f, .09f));
 
         // Hand Held Transforms
-        public static readonly TransformState LeftHand = new(new Vector3(-0.0963f, 0.087f, 0.0238f), Quaternion.Euler(41.609f, 75.893f, 71.86f), new Vector3(.098f, .098f, .098f));
+        public static readonly TransformState LeftHand = new(new Vector3(-0.0963f, 0.087f, 0.0238f), Quaternion.Euler(0f, 270f, 180f), new Vector3(.098f, .098f, .098f));
         public static readonly TransformState RightHand = new(new Vector3(0.1104f, 0.1137f, 0.0552f), Quaternion.Euler(43.8616f, 60.3276f, 329.0535f), new Vector3(.098f, .098f, .098f));
 
         // TO FIX:
