0de173d93054376c34a4215f5c80e6b08159a282
Spankypluh <liamkasia2010@gmail.com>
2025-11-29 21:46:41 -0500

    soon to be added

---
+++ GorillaPad/GorillaPad/Functions/Apps/InboxApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GorillaPad.Interfaces;
+
+namespace GorillaPad.Functions.Apps
+{
+    internal class InboxApp : AppModule
+    {
+        // soon to be added
+        public override string AppName => "Inbox";
+        public override string AppVersion => "0.0.1";
+    }
+}
---
+++ GorillaPad/GorillaPad/Functions/Apps/UpdateApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GorillaPad.Interfaces;
+
+namespace GorillaPad.Functions.Apps
+{
+    internal class UpdateApp : AppModule
+    {
+        // soon to be added
+        public override string AppName => "Update";
+        public override string AppVersion => "0.0.1";
+    }
+}
